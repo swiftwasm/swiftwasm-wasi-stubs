2823ef264c8b4ab503488e21ec03a3c9ca03dead
Yuta Saito <kateinoigakukun@gmail.com>
2019-12-17 06:19:02 +0000

    Add pthread_create stub

--- fakepthread.c
+++ fakepthread.c
@@ -106,6 +106,10 @@ int pthread_detach(pthread_t thread) {
 	STUB();
 }
 
+int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg) {
+	return 0;
+}
+
 // once
 
 int pthread_once(pthread_once_t *once_control, void (*init_routine)(void)) {
